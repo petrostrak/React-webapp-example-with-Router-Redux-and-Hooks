00cfe77059d208cf1369a483103f1332f37df95f
petros <pit.trak@gmail.com>
2020-07-07 17:53:56 +0300

    Added the basic Route Components

--- src/app.js
+++ src/app.js
@@ -1,6 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const DashboardComponent = () => (
+    <div>
+        This is from my DashboardComponent!
+    </div>
+);
+
+
+const CreateComponent = () => (
+    <div>
+        This is from my CreateComponent!
+    </div>
+);
+
+const EditComponent = () => (
+    <div>
+        This is from my EditComponent!
+    </div>
+);
+
+const HelpComponent = () => (
+    <div>
+        This is from my HelpComponent!
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={DashboardComponent} exact={true}/>
+            <Route path="/create" component={CreateComponent}/>
+            <Route path="/edit" component={EditComponent} />
+            <Route path="/help" component={HelpComponent} />
+        </div>
+    </BrowserRouter>    
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
