1c47758c93a7838cc77547733547f40e47faa7da
petros <pit.trak@gmail.com>
2020-07-08 01:13:56 +0300

    Link and NavLink between components
    Use of activeClassName and exact

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, NavLink, Link } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -31,20 +31,33 @@ const HelpComponent = () => (
 
 const ErrorComponent = () => (
     <div>
-        #ERROR404
+        #ERROR404 <Link to="/">Go Home</Link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink><br/>
+        <NavLink to="/create" activeClassName="is-active">Create expense</NavLink><br/>
+        <NavLink to="/edit" activeClassName="is-active">Edit</NavLink><br/>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
 );
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={DashboardComponent} exact={true}/>
-            <Route path="/create" component={CreateComponent}/>
-            <Route path="/edit" component={EditComponent} />
-            <Route path="/help" component={HelpComponent} />
-            <Route component={ErrorComponent} />
-        </Switch>
-    </BrowserRouter>    
+        <di>
+            <Header />
+            <Switch>
+                <Route path="/" component={DashboardComponent} exact={true} />
+                <Route path="/create" component={CreateComponent} />
+                <Route path="/edit" component={EditComponent} />
+                <Route path="/help" component={HelpComponent} />
+                <Route component={ErrorComponent} />
+            </Switch>
+        </di>
+    </BrowserRouter>
 );
 
 ReactDOM.render(routes, document.getElementById('app'));
