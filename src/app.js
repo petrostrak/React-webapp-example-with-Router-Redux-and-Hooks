c15a9f57b51ca9f524acb959daf768a75d519012
petros <pit.trak@gmail.com>
2020-07-08 00:39:28 +0300

    Added error page
    with the help of Swtch element of React

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -29,14 +29,21 @@ const HelpComponent = () => (
     </div>
 );
 
+const ErrorComponent = () => (
+    <div>
+        #ERROR404
+    </div>
+);
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={DashboardComponent} exact={true}/>
             <Route path="/create" component={CreateComponent}/>
             <Route path="/edit" component={EditComponent} />
             <Route path="/help" component={HelpComponent} />
-        </div>
+            <Route component={ErrorComponent} />
+        </Switch>
     </BrowserRouter>    
 );
 
