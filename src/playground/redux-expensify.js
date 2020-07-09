4d0f5ccaa24e5a4f3a6e241d18f3ae4e6b81fe86
petros <pit.trak@gmail.com>
2020-07-09 15:11:01 +0300

    combineReducer()
    first look at combineReducer method with an example

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,8 +1,51 @@
 import {createStore, combineReducers} from 'redux';
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+const expensesReducerDefaultState = []
+
+const expensesReducer = (state = expensesReducerDefaultState, action)=> {
+    switch(action.type){
+        default: return state;
+    }
+};
+
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+}
+
+const filtersReducer = (state = filtersReducerDefaultState, action)=> {
+    switch(action.type){
+        default: return state;
+    }
+};
+
+//CombineReducers() function takes an object as parameter with key-pair values.
+//The key is the root state name and the pair would be the Reducer that is
+//responsible to manage it 
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState())
+
 const demoState = {
-    expenses = [{
-        id:001,
+    expenses : [{
+        id:'001',
         description: 'January rent',
         note: 'This was the final payment of this address',
         amount: 34000,
