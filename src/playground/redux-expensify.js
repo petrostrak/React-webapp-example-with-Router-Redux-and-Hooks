e2840c9074fca3e46ece90f20ca8c192415a5130
petros <pit.trak@gmail.com>
2020-07-09 18:00:58 +0300

    Create/ Remove an object in an array
    in redux using combineReducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,24 @@
 import {createStore, combineReducers} from 'redux';
+import uuid from 'uuid';
 
-// ADD_EXPENSE
+// ADD_EXPENSE action
+const addExpense = ({description = '', note = '', amount = 0, createdAt = 0} = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense:{
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
 // REMOVE_EXPENSE
+const removeExpense = ({id} = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    expense:{
+        id
+    }
+});
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -14,6 +31,8 @@ const expensesReducerDefaultState = []
 
 const expensesReducer = (state = expensesReducerDefaultState, action)=> {
     switch(action.type){
+        case 'ADD_EXPENSE': return [...state, action.expense]
+        case 'REMOVE_EXPENSE': return state.filter(({id})=>id !== action.id)
         default: return state;
     }
 };
@@ -41,7 +60,16 @@ const store = createStore(
     })
 );
 
-console.log(store.getState())
+store.subscribe(()=>{
+    console.log(store.getState())
+})
+
+const expenseOne   =store.dispatch(addExpense({description:'Rent', amount:350}))
+const expenseTwo   =store.dispatch(addExpense({description:'Movies', amount:20, note:"Don't go to Twilight!"}))
+const expenseThree =store.dispatch(addExpense({description:'Super market', amount:45}))
+const expenseFour  =store.dispatch(addExpense({description:'Wow Subscription', amount:12.99}))
+
+store.dispatch(removeExpense({id: expenseOne.expense.id}));
 
 const demoState = {
     expenses : [{
