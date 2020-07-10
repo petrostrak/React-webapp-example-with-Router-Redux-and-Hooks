dcaafe4991b079aa26dd508af1dc45eefe1a70e2
petros <pit.trak@gmail.com>
2020-07-10 10:33:31 +0300

    Organizing redux
    by putting the actions / reducers/ selectors and store to separate folders and files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpense from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'; 
+
+const store = configureStore();
+
+store.dispatch(addExpense({description:'water bill', amount: 60}));
+store.dispatch(addExpense({description:'gas bill', amount:'45'}));
+
+store.dispatch(setTextFilter('water'));
+// store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpense(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
