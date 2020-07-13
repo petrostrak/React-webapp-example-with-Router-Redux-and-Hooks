e9da74b4bd31e8575eef7f927ca4c6b8f365930d
petros <pit.trak@gmail.com>
2020-07-13 10:13:00 +0300

    Testing Expenses Selector

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,52 +1,51 @@
-import {addExpense, editExpense, removeExpense} from '../../actions/expenses';
-import uuid from 'uuid';
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
 
-test('Should setup remove expense action object', ()=>{
-    const action = removeExpense({id:'123abc'});
-    expect(action).toEqual({
-        type: 'REMOVE_EXPENSE',
-        id: '123abc'
-    })
-})
-
-test('Should setup add expense action object with provided values', ()=>{
-    const expenseDate ={
-        description: 'Rent',
-        amount: 34000,
-        createdAt: 1000,
-        note: 'Mays rent'
-    }
-    const action = addExpense(expenseDate);
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense:{
-            ...expenseDate,
-            id: expect.any(String)
-        }
-    })
-});
-
-test('Should setup add expense action object with default values', ()=>{
-    const action = addExpense();
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense:{
-            id: expect.any(String),
-            description: '',
-            amount: 0,
-            createdAt: 0,
-            note: ''
-        }
-    })
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: '123abc' });
+  expect(action).toEqual({
+    type: 'REMOVE_EXPENSE',
+    id: '123abc'
+  });
 });
 
 test('should setup edit expense action object', () => {
-    const action = editExpense('123abc', { note: 'New note value' });
-    expect(action).toEqual({
-      type: 'EDIT_EXPENSE',
-      id: '123abc',
-      updates: {
-        note: 'New note value'
-      }
-    });
+  const action = editExpense('123abc', { note: 'New note value' });
+  expect(action).toEqual({
+    type: 'EDIT_EXPENSE',
+    id: '123abc',
+    updates: {
+      note: 'New note value'
+    }
   });
+});
+
+test('should setup add expense action object with provided values', () => {
+  const expenseData = {
+    description: 'Rent',
+    amount: 109500,
+    createdAt: 1000,
+    note: 'This was last months rent'
+  };
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  });
+});
+
+test('should setup add expense action object with default values', () => {
+  const action = addExpense();
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      id: expect.any(String),
+      description: '',
+      note: '',
+      amount: 0,
+      createdAt: 0
+    }
+  });
+});
