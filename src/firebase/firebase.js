d372d277f7ec49cb240a412d40935de27ec33cb7
petros <pit.trak@gmail.com>
2020-07-14 13:30:23 +0300

    Fetching Data From Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,32 +15,52 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-firebase.database().ref().set({
-    name: 'petros trak',
-    stressLevel: 6,
-    age: 34,
-    isSingle: true,
-    job:{
-        title: 'Software engineer',
-        company: 'Google'
-    },
-    location:{
-        city: 'Athens',
-        country: 'Greece'
-    }
-}).then(()=>{
-    console.log('data is saved')
-}).catch((e)=>{
-    console.log('This failed', e)
+database.ref().on('value', (snapshop)=>{
+    const val = snapshop.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
 })
+
+// database.ref().on('value', (snapshop)=>{
+//     console.log(snapshop.val());
+// });
+
+// setTimeout(()=>{
+// database.ref('age').set(30)
+// }, 3500)
+
+// database.ref('location/city').once('value')
+//     .then((snapshop)=>{
+//         const val = snapshop.val();
+//         console.log(val)
+//     })
+//     .catch((e)=>{console.log('Error fetching data..', e)})
+
+// firebase.database().ref().set({
+//     name: 'petros trak',
+//     stressLevel: 6,
+//     age: 34,
+//     isSingle: true,
+//     job:{
+//         title: 'Software engineer',
+//         company: 'Google'
+//     },
+//     location:{
+//         city: 'Athens',
+//         country: 'Greece'
+//     }
+// }).then(()=>{
+//     console.log('data is saved')
+// }).catch((e)=>{
+//     console.log('This failed', e)
+// })
 
 // database.ref('isSingle').remove()
 //         .then(()=>{console.log('Field successuffuly removed')})
 //         .catch((e)=>{console.log('Something went wrong',e)})
 // database.ref('isSingle').set(null);
 
-database.ref().update({
-    stressLevel: 9,
-    'job/company' : 'Amazon',
-    'location/city': 'Seattle'
-})
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company' : 'Amazon',
+//     'location/city': 'Seattle'
+// })
