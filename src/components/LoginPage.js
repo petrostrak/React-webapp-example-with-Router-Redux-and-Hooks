4aa1ff0b18a332534ee3885126f9d8413c60a9ce
petros <pit.trak@gmail.com>
2020-07-17 19:57:29 +0300

    Styling Buttons
    and Header component

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,8 +6,8 @@ export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">React project</h1>
-      <p>New order!</p>
-      <button onClick={startLogin}>Login</button>
+      <p>Using Router Redux and Hooks</p>
+      <button onClick={startLogin} className="button">Login with Google</button>
     </div>
   </div>
 );
